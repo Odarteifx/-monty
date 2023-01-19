e6e04d25f965213c0c4f58cf1858eb9e97f9b6e0
William Lamptey <williamlamp512@gmail.com>
2023-01-19 14:11:07 -0800

    we dey commit waa

--- monty.h
+++ monty.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define STACK 0
 #define QUEUE 1
